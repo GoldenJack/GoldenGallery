d1eee1aefb72f5f293f6d9b58c0a9fc8bfa6efff
GoldenJack <kazanzev2010@yandex,ru>
2018-11-03 19:18:35 +0300

    Архитектура проекта

---
+++ webpack.config.js
@@ -0,0 +1,97 @@
+let path = require('path');
+let ExtractTextPlugin = require('extract-text-webpack-plugin');
+let CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: [
+        './src/js/index.js',
+        './src/assets/scss/app.scss'
+    ],
+    output: {
+        path: path.resolve(__dirname, './public/'),
+        filename: 'js/bundle.js'
+    },
+
+    devServer: {
+        contentBase: './public',
+        port: 8000
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: '/node_modules/',
+                loader: 'babel-loader'
+            },
+            {
+				test: /\.(sass|scss)$/,
+				include: path.resolve(__dirname, 'src/assets/scss'),
+				use: ExtractTextPlugin.extract({
+					use: [{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true,
+							minimize: true
+						}
+                    },
+                    {
+						loader: 'resolve-url-loader'
+					},
+					{
+						loader: 'sass-loader',
+						options: {
+							sourceMap: true
+						}
+					}]
+				})
+            },
+            {
+				test: /\.(png|jpg|gif|svg)$/,
+				use: {
+					loader: 'file-loader',
+					options: {
+						name: './img/[name].[ext]'
+					}
+				}
+			},
+			{
+				test: /\.(woff|woff2|ttf|otf|eot)$/,
+				use: {
+					loader: 'file-loader',
+					options: {
+						name: './img/[name].[ext]'
+					}
+				}
+			},
+        ]
+    },
+    plugins: [
+		new ExtractTextPlugin({
+			filename: './css/app.min.css',
+			allChunks: true
+        }),
+        new CopyWebpackPlugin([
+			{
+				from: './src/assets/img',
+				to: './img'
+			},
+			{
+				from: './src/assets/fonts',
+				to: './fonts'
+			}
+		]),
+		new HtmlWebpackPlugin({
+			title: 'index',
+			filename: 'index.html',
+			template: './src/assets/html/index.html'
+		}),
+		new HtmlWebpackPlugin({
+			title: 'home',
+			filename: 'Home.html',
+			template: './src/Home.html'
+		})
+	],
+    devtool: "source-map"
+}
